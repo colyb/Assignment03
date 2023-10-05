9da8f25dd15479d39fe0da5764cd04f178a35d11
Co Bai Ly <col@uci.edu>
2023-10-04 20:16:50 -0700

    fixed coinflip

--- js/01-coinflip-redux.js
+++ js/01-coinflip-redux.js
@@ -19,7 +19,7 @@ let numOfFlip = parseInt(prompt("Please enter the amount of time to flip the coi
 
 for (let i = 0; i < numOfFlip; i++)
 {
-    let coinFlip = Math.round(Math.random())
+    coinFlip = Math.round(Math.random())
     coinFlip === 0 ? document.write("Heads\n") : document.write("Tails\n")
 }
 
